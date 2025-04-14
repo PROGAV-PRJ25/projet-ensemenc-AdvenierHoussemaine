caf18ef1a1c1e45db3eac6fe8d0ef18d72ee0738
flor-issons <flore.advenier@gmail.com>
2025-04-14 12:21:22 +0200

    debut creation grilles et affichage

---
+++ Programme/Affichage/Affichage.cs
@@ -0,0 +1,28 @@
+/*
+new class Affichage ()
+{
+    List <List<string>> Grille {get; set;}
+    List <Terrains> TerrainsAfficher {get; set;}
+
+    public Affichage()
+    {
+        //Crée une grille vide de dimension 2x3 : dans chaque case il y a 6 espaces pour planter une plante
+        Grille = new List<List<string>>();
+        for (int i=0; i<13; i++)
+        {
+            for (int j=0; j<7; j++)
+            {
+                if (j==0 || j==3) Grille[i][j] = "_";
+                else if (i%3 == 0) Grille[i][j] = "|";
+                else Grille[i][j] = " ";
+            }
+        }
+    }
+    public override string ToString()
+    {
+        //Crée une grille vide 
+
+        return affichage;
+    }
+}
+*/
