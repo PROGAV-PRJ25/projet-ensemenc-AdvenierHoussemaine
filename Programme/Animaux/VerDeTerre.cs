0ab15d0ed106edb4d7c1a201342b062b22cffaba
phoussemain <phoussemain@ensc.fr>
2025-05-15 15:57:37 +0200

    Premiere version fini de calsse VerDeTerre

--- Programme/Animaux/VerDeTerre.cs
+++ Programme/Animaux/VerDeTerre.cs
@@ -9,6 +9,14 @@ public class VerDeTerre : Animaux
 
     public override void Action(int parcelleDepart, int emplacementDepart) //les ver de terre augmentent l'humidité de la parcelle 
     {
+        if (TerrainAnimal.Parcelles[parcelleDepart].AbsorbtionDeLeau<=0.9)
+        {
+            TerrainAnimal.Parcelles[parcelleDepart].AbsorbtionDeLeau+=0.1;
+        }
+    }
 
+    public override string ToString()
+    {
+        return $"Des vers de terre ont augmenté la capacité d'absorbtion de l'eau de la parcelle {ParcellePositionAnimal +1} . \n Elle est maintenant de {TerrainAnimal.Parcelles[ParcellePositionAnimal].AbsorbtionDeLeau} ";
     }
 }
